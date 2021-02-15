cf189cb41e6899f01141aacf38e423274b8c48cf
gngn <guillaume.martin@eemi.com>
2021-02-15 14:47:55 +0100

    trad FR

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -22,8 +22,8 @@ class UserType extends AbstractType
             ->add('username', TextType::class)
             ->add('plainPassword', RepeatedType::class, array(
                 'type' => PasswordType::class,
-                'first_options'  => array('label' => 'Password'),
-                'second_options' => array('label' => 'Repeat Password')
+                'first_options'  => array('label' => 'Mot de passe'),
+                'second_options' => array('label' => 'Répéter le mot de passe')
             ))
         ;
     }
